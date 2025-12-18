a46a8a0e52c0a22ee5a5d00ab40dff2d21a9d354
mrwolf09 <85397943+mrwolf09@users.noreply.github.com>
2025-12-18 12:15:11 +0530

    Refactor bot commands to use 'bot' instance

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,43 +1,48 @@
 from pyrogram import filters
-from pyrogram import Client as stark
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from main import LOGGER, PREFIXES, AUTH_USERS
+
+from main import bot, LOGGER, PREFIXES, AUTH_USERS
 from config import Config
+
 import os
 import sys
 
 
-@stark.on_message(filters.command(["start"]) & ~filters.edited)
-async def Start_msg(bot: stark , m: Message):
+@bot.on_message(filters.command(["start"]) & ~filters.edited)
+async def Start_msg(_, m: Message):
     await bot.send_photo(
-    m.chat.id,
-    photo="https://telegra.ph/file/cef3ef6ee69126c23bfe3.jpg",
-    caption = "**Hi i am All in One Extractor Bot**.\n"
-                            "Press **/pw** for **Physics Wallah**..\n\n"
-                            "Press **/e1** for **E1 Coaching App**..\n\n"
-                            "Press **/vidya** for **Vidya Bihar App**..\n\n"
-                            "Press **/ocean** for **Ocean Gurukul App**..\n\n"
-                            "Press **/winners** for **The Winners Institute**..\n\n"
-                            "Press **/rgvikramjeet** for **Rgvikramjeet App**..\n\n"
-                            "Press **/txt** for  **Ankit With Rojgar,**\n**The Mission Institute,**\n**The Last Exam App**..\n\n"
-                            "Press **/cp** for **classplus appp**..\n\n"
-                            "Press **/cw** for **careerwill app**..\n\n"
-                            "Press **/khan** for **Khan Gs app**..\n\n"
-                            "Press **/exampur** for ** Exampur app**..\n\n"
-                            "Press **/samyak** for ** Samayak Ias**..\n\n"
-                            "Press **/mgconcept** for **Mgconcept app**..\n\n"
-                            "Press **/down** for **For Downloading Url lists**..\n\n"
-                            "Press **/forward** To **Forward from One channel to others**..\n\n"
-                            "**𝗕𝗼𝘁 𝗢𝘄𝗻𝗲𝗿 : 𝒞𝓇𝓎𝓅𝓉💞𝓈𝓉𝒶𝓇𝓀**")
-           
+        m.chat.id,
+        photo="https://telegra.ph/file/cef3ef6ee69126c23bfe3.jpg",
+        caption=(
+            "**Hi i am All in One Extractor Bot**.\n\n"
+            "Press **/pw** for **Physics Wallah**..\n\n"
+            "Press **/e1** for **E1 Coaching App**..\n\n"
+            "Press **/vidya** for **Vidya Bihar App**..\n\n"
+            "Press **/ocean** for **Ocean Gurukul App**..\n\n"
+            "Press **/winners** for **The Winners Institute**..\n\n"
+            "Press **/rgvikramjeet** for **Rgvikramjeet App**..\n\n"
+            "Press **/txt** for  **Ankit With Rojgar,**\n"
+            "**The Mission Institute,**\n"
+            "**The Last Exam App**..\n\n"
+            "Press **/cp** for **classplus app**..\n\n"
+            "Press **/cw** for **careerwill app**..\n\n"
+            "Press **/khan** for **Khan Gs app**..\n\n"
+            "Press **/exampur** for **Exampur app**..\n\n"
+            "Press **/samyak** for **Samyak Ias**..\n\n"
+            "Press **/mgconcept** for **Mgconcept app**..\n\n"
+            "Press **/down** for **For Downloading Url lists**..\n\n"
+            "Press **/forward** To **Forward from One channel to others**..\n\n"
+            "**𝗕𝗼𝘁 𝗢𝘄𝗻𝗲𝗿 : 𝒞𝓇𝓎𝓅𝓉💞𝓈𝓉𝒶𝓇𝓀**"
+        )
+    )
 
 
-@stark.on_message(filters.command(["restart"]) & ~filters.edited)
-async def restart_handler(_, m):
-    await m.reply_text("Restarted!", True)
+@bot.on_message(filters.command(["restart"]) & ~filters.edited)
+async def restart_handler(_, m: Message):
+    await m.reply_text("Restarting...")
     os.execl(sys.executable, sys.executable, *sys.argv)
 
-@stark.on_message(filters.command(["log"]) & ~filters.edited)
-async def log_msg(bot: stark , m: Message):   
-    await bot.send_document(m.chat.id, "log.txt")
 
+@bot.on_message(filters.command(["log"]) & ~filters.edited)
+async def log_msg(_, m: Message):
+    await bot.send_document(m.chat.id, "log.txt")
